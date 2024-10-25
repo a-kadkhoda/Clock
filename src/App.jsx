74d9833455cbcff17690020ce89f2c035de3db00
a-kadkhoda <a-kadkhoda@users.noreply.github.com>
2024-10-25 17:06:49 +0330

    v1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Classic from "./components/Classic";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex justify-center items-center bg-red-200">
+        <Classic />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Classic.jsx
@@ -0,0 +1,53 @@
+import { useRef } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Classic = () => {
+  const [time, setTime] = useState(new Date());
+  const clockRef = useRef(null)
+  useEffect(() => {
+
+
+    return()=>{
+        
+    }
+  }, []);
+
+  const Clock = ()=>{
+    clockRef.current = setInterval(() => {
+        setTime(new Date());
+      }, 1000);
+  }
+
+  return (
+    <>
+      <div className="relative w-[300px] h-[300px] bg-white rounded-full border-4  border-black">
+        <span className="absolute font-bold top-[2%] left-[47%]">12</span>
+        <span className="absolute font-bold bottom-[2%] left-[48%]">6</span>
+        <span className="absolute font-bold left-[2%] top-[46%]">9</span>
+        <span className="absolute font-bold right-[2%] top-[46%]">3</span>
+        <div className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-3 h-3 rounded-full bg-gray-800 shadow-xl z-50"></div>
+        <div
+          className="absolute top-[50%] left-[49.25%] -translate-x-1/2 -translate-y-1/2 w-1 h-16 bg-black rounded-br-lg rounded-bl-lg transform origin-top "
+          style={{
+            transform: `rotateZ(${time.getHours() * 30}deg)`,
+          }}
+        ></div>
+        <div
+          className="absolute top-[50%] left-[49.25%] -translate-x-1/2 -translate-y-1/2 w-1 h-24 bg-black rounded-br-lg rounded-bl-lg transform origin-top "
+          style={{
+            transform: `rotateZ(${time.getMinutes() * 6}deg)`,
+          }}
+        ></div>
+        <div
+          className="absolute top-[50%] left-[49.25%] -translate-x-1/2 -translate-y-1/2 w-1 h-28 bg-red-600 rounded-br-lg rounded-bl-lg transform origin-top "
+          style={{
+            transform: `rotateZ(${time.getSeconds() * 6}deg)`,
+          }}
+        ></div>
+      </div>
+    </>
+  );
+};
+
+export default Classic;
