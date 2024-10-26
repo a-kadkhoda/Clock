54d9ce3981ff22ebfa3e31913fba8d4f44aaf1aa
a-kadkhoda <a-kadkhoda@users.noreply.github.com>
2024-10-26 12:05:01 +0330

    Update Classic.jsx

--- src/components/Classic.jsx
+++ src/components/Classic.jsx
@@ -22,19 +22,19 @@ const Classic = () => {
         <span className="absolute font-bold right-[2%] top-[46%]">3</span>
         <div className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-3 h-3 rounded-full bg-gray-800 shadow-xl z-50"></div>
         <div
-          className="absolute top-[28%] left-[49.25%] -translate-x-1/2 -translate-y-1/2 w-1 h-16 bg-black rounded-br-lg rounded-bl-lg transform origin-bottom "
+          className="absolute top-[28%] left-[49.25%] -translate-x-1/2 -translate-y-1/2 w-1 h-16 bg-black rounded-tr-lg rounded-tl-lg transform origin-bottom "
           style={{
             transform: `rotateZ(${time.getHours() * 30}deg)`,
           }}
         ></div>
         <div
-          className="absolute top-[17%] left-[49.25%] -translate-x-1/2 -translate-y-1/2 w-1 h-24 bg-black rounded-br-lg rounded-bl-lg transform origin-bottom "
+          className="absolute top-[17%] left-[49.25%] -translate-x-1/2 -translate-y-1/2 w-1 h-24 bg-black rounded-tr-lg rounded-tl-lg transform origin-bottom "
           style={{
             transform: `rotateZ(${time.getMinutes() * 6}deg)`,
           }}
         ></div>
         <div
-          className="absolute top-[11%] left-[49.25%] -translate-x-1/2 -translate-y-1/2 w-1 h-28 bg-red-600 rounded-br-lg rounded-bl-lg transform origin-bottom "
+          className="absolute top-[11%] left-[49.25%] -translate-x-1/2 -translate-y-1/2 w-1 h-28 bg-red-600 rounded-tr-lg rounded-tl-lg transform origin-bottom "
           style={{
             transform: `rotateZ(${time.getSeconds() * 6}deg)`,
           }}
